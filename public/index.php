d744b9e6827f7ee2bb2e540acdbd5227e298de33
Mikail <miklsuhanov@gmail.com>
2023-05-26 13:15:30 +1000

    add index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
-echo 'kek';
-?>
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+
+$app->run();
