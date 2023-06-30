0a6190633f7c0551d33fd1bac859556aaa772a79
Mikail <miklsuhanov@gmail.com>
2023-06-30 13:00:28 +1000

    change Connection.php

--- app/Connection.php
+++ app/Connection.php
@@ -10,18 +10,35 @@ final class Connection
     {
         $params = parse_ini_file('Database.ini');
         if ($params === false) {
-
-            throw new \Exception("Error reading database configuration file");
+            $databaseUrl = parse_url($_ENV['DATABASE_URL']);
+            $username = $databaseUrl['user']; // janedoe
+            $password = $databaseUrl['pass']; // mypassword
+            $host = $databaseUrl['host']; // localhost
+            $port = $databaseUrl['port']; // 5432
+            $dbName = ltrim($databaseUrl['path'], '/');
+            $conStr = sprintf(
+                "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
+                $host,
+                $port,
+                $dbName,
+                $username,
+                $password
+            );
+        }else {
+            $params = parse_ini_file('Database.ini');
+            if ($params) {
+                $conStr = sprintf(
+                    "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
+                    $params['host'],
+                    $params['port'],
+                    $params['database'],
+                    $params['user'],
+                    $params['password']
+                );
+            } else {
+                return false;
+            }
         }
-
-        $conStr = sprintf(
-            "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
-            $params['host'],
-            $params['port'],
-            $params['database'],
-            $params['user'],
-            $params['password']
-        );
 
         $pdo = new \PDO($conStr);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
