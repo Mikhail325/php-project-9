8e96ea036ab9371a9ebfa23bc816ab517fad27a0
Mikail <miklsuhanov@gmail.com>
2023-07-19 12:57:16 +1000

    create test

--- app/Connection.php
+++ app/Connection.php
@@ -8,20 +8,38 @@ final class Connection
 
     public function connect()
     {
-        $params = parse_ini_file('Database.ini');
-        if ($params === false) {
-            throw new \Exception("Error reading database configuration file");
-        }
-
-        $conStr = sprintf(
-            "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
-            $params['host'],
-            $params['port'],
-            $params['database'],
-            $params['user'],
-            $params['password']
-        );
-
+        $file = realpath(__DIR__ . '/Database.ini');
+        if ($file === false) {
+                $databaseUrl = parse_url($_ENV['DATABASE_URL']);
+                $username = $databaseUrl['user']; // janedoe
+                $password = $databaseUrl['pass']; // mypassword
+                $host = $databaseUrl['host']; // localhost
+                $port = $databaseUrl['port']; // 5432
+                $dbName = ltrim($databaseUrl['path'], '/');
+                $conStr = sprintf(
+                    "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
+                    $host,
+                    $port,
+                    $dbName,
+                    $username,
+                    $password
+                );
+            } else {
+                $params = parse_ini_file('Database.ini');
+                if ($params) {
+                    $conStr = sprintf(
+                        "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
+                        $params['host'],
+                        $params['port'],
+                        $params['database'],
+                        $params['user'],
+                        $params['password']
+                    );
+                } else {
+                    return false;
+                }
+            }
+        
         $pdo = new \PDO($conStr);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $pdo;
---
+++ tests/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hexlet\Code\tests;
+
+use PHPUnit\Framework\TestCase;
+
+
+class GenDiffTest extends TestCase
+{
+   
+}
