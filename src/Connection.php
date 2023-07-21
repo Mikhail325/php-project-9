605c26084099f4b3f28103bef025eb79c0e05a2a
Mikail <miklsuhanov@gmail.com>
2023-07-21 15:01:59 +1000

    4

--- src/Connection.php
+++ src/Connection.php
@@ -8,25 +8,16 @@ final class Connection
 
     public function connect()
     {
-        $file = realpath(__DIR__ . '/database.ini');
-        if ($file === false) {
-                $databaseUrl = parse_url($_ENV('DATABASE_URL'));
-                $username = $databaseUrl['user']; // janedoe
-                $password = $databaseUrl['pass']; // mypassword
-                $host = $databaseUrl['host']; // localhost
-                $port = $databaseUrl['port']; // 5432
-                $dbName = ltrim($databaseUrl['path'], '/');
-                $conStr = sprintf(
-                    "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
-                    $host,
-                    $port,
-                    $dbName,
-                    $username,
-                    $password
-                );
+        
+        if (isset($_ENV['DATABASE_URL'])) {
+            $databaseUrl = parse_url($_ENV['DATABASE_URL']);
+            $username = $databaseUrl['user'];
+            $password = $databaseUrl['pass'];
+            $host = $databaseUrl['host'];
+            $port = $databaseUrl['port'];
+            $dbname = ltrim($databaseUrl['path'], '/');
         } else {
             $params = parse_ini_file('database.ini');
-            if ($params) {
                 $conStr = sprintf(
                     "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
                     $params['host'],
@@ -37,9 +28,7 @@ final class Connection
                 );
                 $username = $params['user'];
                 $password = $params['password'];
-            }
         }
-
         $pdo = new \PDO($conStr, $username, $password);
         return $pdo;
     }
